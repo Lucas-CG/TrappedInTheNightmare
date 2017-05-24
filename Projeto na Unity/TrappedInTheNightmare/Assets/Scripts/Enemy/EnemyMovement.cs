db19f1ea6a2dc5eba4d1c3beaf240ba90221bb3f
Erik Tronkos <erik.tronkos@poli.ufrj.br>
2017-05-24 20:32:14 -0300

    ia

--- Projeto na Unity/TrappedInTheNightmare/Assets/Scripts/Enemy/EnemyMovement.cs
+++ Projeto na Unity/TrappedInTheNightmare/Assets/Scripts/Enemy/EnemyMovement.cs
@@ -8,8 +8,11 @@ public class EnemyMovement : MonoBehaviour
     EnemyHealth enemyHealth;
     UnityEngine.AI.NavMeshAgent nav;
     public float range = 10.0f;
+	public float fieldOfViewRange; // in degrees (I use 68, this gives the enemy a vision of 136 degrees)
+	public float minPlayerDetectDistance; // the distance the player can come behind the enemy without being deteacted
+	public float rayRange; // distance the enemy can "see" in front of him
 
-    private Vector3 originPosition;
+	private Vector3 originPosition;
 
 
     void Awake ()
@@ -26,19 +29,52 @@ public class EnemyMovement : MonoBehaviour
     {
         float distance = Vector3.Distance(transform.position, player.position);
 
-        if(enemyHealth.currentHealth > 0 && playerHealth.currentHealth > 0 && distance < range)
+		if(enemyHealth.currentHealth > 0 && playerHealth.currentHealth > 0 && distance < range && CanSeePlayer())
         {
             nav.SetDestination (player.position);
         }
 
-        else if (enemyHealth.currentHealth > 0 && transform.position != originPosition)
+		else if (enemyHealth.currentHealth > 0 && Vector3.Distance(transform.position ,originPosition) > 1)
         {
             nav.SetDestination(originPosition);
         }
 
         else
         {
-            nav.enabled = false;
+			transform.Rotate (Vector3.up * Time.deltaTime * 20, Space.World);
         }
     }
+		
+
+	private Vector3 rayDirection = Vector3.zero;
+
+
+	bool CanSeePlayer() 
+	{
+		RaycastHit hit;
+		rayDirection = player.transform.position - transform.position;
+		var distanceToPlayer = Vector3.Distance(transform.position, player.transform.position);
+
+
+		if(Physics.Raycast (transform.position, rayDirection, out hit)){ // If the player is very close behind the enemy and not in view the enemy will detect the player
+			if((hit.transform.tag == "Player") && (distanceToPlayer < minPlayerDetectDistance)){
+				//Debug.Log("Caught player sneaking up behind!");
+				return true;
+			}
+		}
+		if((Vector3.Angle(rayDirection, transform.forward)) < fieldOfViewRange){ // Detect if player is within the field of view
+			if (Physics.Raycast (transform.position, rayDirection, out hit, rayRange)) {
+				if (hit.transform.tag == "Player") {
+					//Debug.Log("Can see player");
+					return true;
+				}else{
+					//Debug.Log("Can not see player");
+					return false;
+				}
+			}
+		}
+		return false;
+	}
+
+
 }
