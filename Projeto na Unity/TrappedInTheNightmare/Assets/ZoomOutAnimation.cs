28242bb3b9725560403cd802ad19d4411b933f5d
Lucas-CG <lucas.gomes@poli.ufrj.br>
2017-07-10 01:00:38 -0300

    câmeras adicionadas para zoom out do mapa e seguir o monstro

---
+++ Projeto na Unity/TrappedInTheNightmare/Assets/ZoomOutAnimation.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZoomOutAnimation : MonoBehaviour {
+
+
+	// Update is called once per frame
+	void Update () {
+        GetComponentInChildren<Camera>().orthographicSize += (35 * Time.deltaTime);
+	}
+}
